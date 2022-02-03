ba4ff6d8162601962393721d18bbd8514680576d
Brian Widgeon <b-widg@users.noreply.github.com>
2022-02-03 15:24:34 -0800

    fix static path

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ app.use(express.json());
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views/pages'));
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
+// app.use('/', express.static(path.join(__dirname, 'public')));
 
 let store = fromJS({
   rovers: [
